4958fca7a705b462142df716449b4dd603c07ea0
Laura <laura.ricchi@users.noreply.github.com>
2020-05-11 00:12:33 +0200

    css uploaded

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,8 +6,8 @@ import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import Grid from "@material-ui/core/Grid";
-import { styled } from "@material-ui/core/styles";
 import Loading from "../components/Loading";
+import { styled } from "@material-ui/core/styles";
 
 const Home = () => {
   // création des états pour l'affichage des profils
